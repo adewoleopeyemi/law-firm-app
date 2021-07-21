8792ffeb995d1e603dbd6c2784e76126b28040b3
adewoleopeyemi <adewoleopw@gmail.com>
2021-07-21 15:17:32 +0100

    offToLoginAndSignUp

--- app/src/main/java/com/example/successsynergyapp/auth/UserTypeActivity.java
+++ app/src/main/java/com/example/successsynergyapp/auth/UserTypeActivity.java
@@ -1,14 +1,46 @@
 package com.example.successsynergyapp.auth;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
 import com.example.successsynergyapp.R;
+import com.example.successsynergyapp.databinding.ActivityUserTypeBinding;
 
 public class UserTypeActivity extends AppCompatActivity {
-
+    ActivityUserTypeBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user_type);
+        getSupportActionBar().hide();
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_user_type);
+        initViews(binding);
+    }
+
+    private void initViews(ActivityUserTypeBinding binding){
+        binding.btnServiceProvider.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(UserTypeActivity.this, LoginActivity.class);
+                i.putExtra("for", "User");
+                startActivity(i);
+            }
+        });
+        binding.btnUserLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(UserTypeActivity.this, LoginActivity.class);
+                i.putExtra("for", "ServiceProvider");
+                startActivity(i);
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        finishAffinity();
     }
 }
